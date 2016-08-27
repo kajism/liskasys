b12a0183b9e23095b0264a487e7cda9e012a8f1b
Karel Miarka <karel.miarka@seznam.cz>
2016-08-27 21:17:03 +0200

    little fixes, refactoring and improvements

--- src/liskasys/service.clj
+++ src/liskasys/service.clj
@@ -23,25 +23,24 @@
   (and (:lunch? att-day)
        (not (:lunch-cancelled? att-day))))
 
-(defn- select-lunch-counts [db-spec date]
+(defn- select-lunch-counts-by-diet-label [db-spec date]
   (let [lunch-types (->> (jdbc-common/select db-spec :lunch-type {})
                          (into [{:id nil :label "běžná"}])
                          (map (juxt :id :label))
                          (into {}))]
     (->> (db/select-children-with-attendance-day db-spec date)
          (filter att-day-with-lunch?)
-         (group-by (comp :lunch-type-id))
+         (group-by :lunch-type-id)
          (map (fn [[k v]] [(get lunch-types k) (count v)]))
          (sort-by first cz-collator ))))
 
 (defn send-lunch-order [db-spec date]
-  (let [lunch-counts (not-empty (select-lunch-counts db-spec date))
+  (let [lunch-counts (not-empty (select-lunch-counts-by-diet-label db-spec date))
         subject (str "Objednávka obědů pro Lištičku na " (->> date
                                                               tc/to-date-time
                                                               (tf/unparse day-formatter)))
-        tos (db/select-users-with-role db-spec "obedy")
         msg {:from "daniela.chaloupkova@post.cz"
-             :to tos
+             :to (mapv :email (db/select-users-with-role db-spec "obedy"))
              :subject subject
              :body [{:type "text/plain; charset=utf-8"
                      :content (str subject "\n"
@@ -51,12 +50,13 @@
                                           (for [[t c] lunch-counts]
                                             (str t ": " c "\n")))
                                    "-------------------------------------------------\n"
-                                   "Celkem: " (apply + (map second lunch-counts)))}]}]
+                                   "CELKEM: " (apply + (map second lunch-counts)))}]}]
     (if-not lunch-counts
-      (timbre/info "No lunches for tomorrow. Sending skipped.")
-      (try
-        (timbre/info "Sending " (:subject msg) "to" (:tos msg))
-        (->> (postal/send-message msg)
-             (timbre/info "Lunch order sent"))
-        (catch Exception e
-          (timbre/error "Error sending email" e))))))
+      (timbre/info "No lunches for " date ". Sending skipped.")
+      (do
+        (timbre/info "Sending " (:subject msg) "to" (:to msg))
+        (timbre/debug msg)
+        (let [result (postal/send-message msg)]
+          (if (:error result)
+            (timbre/error "Failed to send email" result)
+            (timbre/info "Lunch order has been sent" result)))))))
