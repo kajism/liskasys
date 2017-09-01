ed12ed9dcad23d8fe7e477e8177b950ed5e84e0a
Karel Miarka <karel.miarka@seznam.cz>
2017-09-01 11:06:15 +0200

    lunch ordering moved from 10AM to 9AM

--- src/liskasys/component/scheduler.clj
+++ src/liskasys/component/scheduler.clj
@@ -24,19 +24,19 @@
       ;; cron expression: sec min hour day-of-mon mon day-of-week ?year
       (close-lunch-order-job sched
                              [(:conn datomic)]
-                             :trigger {:cron {:expression "0 1 10 * * ?" ;; at 10:01:00 AM
+                             :trigger {:cron {:expression "0 1 9 * * ?" ;; at 9:01:00 AM
                                               :misfire-handling :fire-and-process
                                               :time-zone (TimeZone/getTimeZone "Europe/Prague")}})
 
       (process-substitutions-job sched
                              [(:conn datomic)]
-                             :trigger {:cron {:expression "30 1 10 * * ?" ;; at 10:01:30 AM
+                             :trigger {:cron {:expression "30 1 9 * * ?" ;; at 9:01:30 AM
                                               :misfire-handling :fire-and-process
                                               :time-zone (TimeZone/getTimeZone "Europe/Prague")}})
 
       (process-lunch-order-job sched
                             [(:conn datomic)]
-                            :trigger {:cron {:expression "0 2 10 * * ?" ;; at 10:02:00 AM
+                            :trigger {:cron {:expression "0 2 9 * * ?" ;; at 9:02:00 AM
                                              :misfire-handling :fire-and-process
                                              :time-zone (TimeZone/getTimeZone "Europe/Prague")}})
       (assoc component :twarc-scheduler sched)))
