7f0e1cd4bf2504703880fced9caa40d993e5dcf5
Karel Miarka <karel.miarka@seznam.cz>
2017-09-07 15:21:23 +0200

    Version 1.0.28-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject liskasys "1.0.27"
+(defproject liskasys "1.0.28-SNAPSHOT"
   :description "Web information, attendance and lunch cancelation/ordering system for a forest kidergarten"
   :url "http://obedy.listicka.org"
   :min-lein-version "2.0.0"
