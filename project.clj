04e955eaaf70fe5f57c514d8a8a097eb3e340b6f
Karel Miarka <karel.miarka@seznam.cz>
2018-01-26 15:12:37 +0100

    fixes version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject liskasys "1.0.50"
+(defproject liskasys "1.0.51-SNAPSHOT"
   :description "Web information, attendance and lunch cancelation/ordering system for a forest kidergarten"
   :url "https://github.com/kajism/liskasys"
   :min-lein-version "2.0.0"
