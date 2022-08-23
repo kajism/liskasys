a41527c6f12d2c4a0c4f421134ba3d6d2f22867d
Karel Miarka <karel.miarka@seznam.cz>
2022-08-23 14:06:24 +0200

    Migration do deps.edn, integrant system and shadow-cljs builds.

--- test/liskasys/emailing_test.clj
+++ test/liskasys/emailing_test.clj
@@ -1,6 +1,6 @@
 (ns liskasys.emailing-test
-  (:require [clojure.test :refer :all]
-            [liskasys.emailing :refer :all]))
+  (:require [clojure.test :refer [deftest is]]
+            [liskasys.emailing :as sut]))
 
 (def diet-labels-by-id {nil "běžná", 2 "lactose free", 3 "gluten free"})
 
@@ -16,12 +16,12 @@
 
 (deftest all-diet-labels-by-id-test
   (is (= diet-labels-by-id
-         (all-diet-labels-by-id [{:db/id 2 :lunch-type/label "lactose free"}
+         (sut/all-diet-labels-by-id [{:db/id 2 :lunch-type/label "lactose free"}
                                  {:db/id 3 :lunch-type/label "gluten free"}]))))
 
 (deftest lunch-counts-by-diet-label-test
   (is (= {"běžná" 7 "lactose free" 2 "gluten free" 1}
-         (lunch-counts-by-diet-label diet-labels-by-id
+         (sut/lunch-counts-by-diet-label diet-labels-by-id
                                      plans-with-lunches))))
 
 (deftest lunch-order-msg-test
@@ -44,7 +44,7 @@
   lactose free: 1
 -------------------------------------------------
 CELKEM: 10\n\n"}]}
-         (lunch-order-msg "me@test-school.org"
+         (sut/lunch-order-msg "me@test-school.org"
                           ["someone@yahoo.com" "anotherone@gmail.com"]
                           "Test school"
                           diet-labels-by-id
@@ -53,23 +53,23 @@ CELKEM: 10\n\n"}]}
 
 (deftest name-att-diet-str-test
   (is (= "Miarka Oliver, bez oběda"
-         (name-att-diet-str {:daily-plan/person {:person/firstname "Oliver" :person/lastname "Miarka"}} )))
+         (sut/name-att-diet-str {:daily-plan/person {:person/firstname "Oliver" :person/lastname "Miarka"}} )))
   (is (= "Miarka Oliver"
-         (name-att-diet-str {:daily-plan/person {:person/firstname "Oliver" :person/lastname "Miarka"}
+         (sut/name-att-diet-str {:daily-plan/person {:person/firstname "Oliver" :person/lastname "Miarka"}
                              :daily-plan/lunch-req 1})))
   (is (= "Miarka Oliver, strava vege"
-         (name-att-diet-str {:daily-plan/person {:person/firstname "Oliver" :person/lastname "Miarka" :person/lunch-type {:lunch-type/label "vege"}}
+         (sut/name-att-diet-str {:daily-plan/person {:person/firstname "Oliver" :person/lastname "Miarka" :person/lunch-type {:lunch-type/label "vege"}}
                              :daily-plan/lunch-req 1})))
   (is (= "Miarka Oliver, půldenní"
-         (name-att-diet-str {:daily-plan/person {:person/firstname "Oliver" :person/lastname "Miarka"}
+         (sut/name-att-diet-str {:daily-plan/person {:person/firstname "Oliver" :person/lastname "Miarka"}
                              :daily-plan/child-att 2
                              :daily-plan/lunch-req 1}))))
 
 (deftest name-excuse-str-test
   (is (= "Miarka Oliver"
-         (name-excuse-str {:daily-plan/person {:person/firstname "Oliver" :person/lastname "Miarka"}})))
+         (sut/name-excuse-str {:daily-plan/person {:person/firstname "Oliver" :person/lastname "Miarka"}})))
   (is (= "Miarka Oliver, skiing"
-         (name-excuse-str {:daily-plan/person {:person/firstname "Oliver" :person/lastname "Miarka"}
+         (sut/name-excuse-str {:daily-plan/person {:person/firstname "Oliver" :person/lastname "Miarka"}
                            :daily-plan/excuse "skiing"}))))
 
 (deftest today-child-counts-msg-test
@@ -80,7 +80,7 @@ CELKEM: 10\n\n"}]}
           [{:type "text/plain; charset=utf-8",
             :content
             "Test school: Celkový dnešní počet dětí je 3\n\nfirst: 2\nsecond: 1\n\n\nToto je automaticky generovaný email ze systému http://testschool.org/liskasys"}]}
-         (today-child-counts-msg "me@test-school.org"
+         (sut/today-child-counts-msg "me@test-school.org"
                                  ["someone@yahoo.com" "anotherone@gmail.com"]
                                  config
                                  [{:db/id 1 :group/label "first"}
@@ -108,7 +108,7 @@ Splatnost do: do konce roku
 Pro QR platbu přejděte na http://testschool.org/liskasys menu Platby
 
 Toto je automaticky generovaný email ze systému http://testschool.org/liskasys"}]}
-         (bill-published-msg "me@test-school.org"
+         (sut/bill-published-msg "me@test-school.org"
                              config
                              {:price-list/bank-account "123456/7890"}
                              "do konce roku"
@@ -145,7 +145,7 @@ Do poznámky: Miarka Oliver 2019/01 - 2019/02
 Splatnost do: do konce roku
 
 Toto je automaticky generovaný email ze systému http://testschool.org/liskasys"}]}
-         (bill-published-msg "me@test-school.org"
+         (sut/bill-published-msg "me@test-school.org"
                              config
                              {:price-list/bank-account "123456/7890"
                               :price-list/bank-account-lunches "222222/4444"}
@@ -167,7 +167,7 @@ Toto je automaticky generovaný email ze systému http://testschool.org/liskasys
           [{:type "text/plain; charset=utf-8",
             :content
             "Miarka Oliver má zítra ve školce nahradní celodenní docházku bez oběda.\n\nToto je automaticky generovaný email ze systému http://testschool.org/liskasys"}]}
-         (substitution-result-msg "me@test-school.org"
+         (sut/substitution-result-msg "me@test-school.org"
                                   config
                                   {:daily-plan/person {:person/firstname "Oliver" :person/lastname "Miarka"
                                                        :person/parent [{:person/email "someone@yahoo.com"}
@@ -182,7 +182,7 @@ Toto je automaticky generovaný email ze systému http://testschool.org/liskasys
           [{:type "text/plain; charset=utf-8",
             :content
             "Miarka Oliver má zítra ve školce nahradní půldenní docházku včetně oběda.\n\nToto je automaticky generovaný email ze systému http://testschool.org/liskasys"}]}
-         (substitution-result-msg "me@test-school.org"
+         (sut/substitution-result-msg "me@test-school.org"
                                   config
                                   {:daily-plan/person {:person/firstname "Oliver" :person/lastname "Miarka"
                                                        :person/parent [{:person/email "someone@yahoo.com"}
@@ -198,7 +198,7 @@ Toto je automaticky generovaný email ze systému http://testschool.org/liskasys
           [{:type "text/plain; charset=utf-8",
             :content
             "Miarka Oliver si bohužel zítra nemůže nahradit docházku z důvodu nedostatku volných míst.\n\nToto je automaticky generovaný email ze systému http://testschool.org/liskasys"}]}
-         (substitution-result-msg "me@test-school.org"
+         (sut/substitution-result-msg "me@test-school.org"
                                   config
                                   {:daily-plan/person {:person/firstname "Oliver" :person/lastname "Miarka"
                                                        :person/parent [{:person/email "someone@yahoo.com"}
--- test/liskasys/service_test.clj
+++
@@ -1,5 +0,0 @@
-(ns liskasys.service-test
-  (:require [clojure.test :refer :all]
-            [liskasys.service :refer :all]
-            ))
-
